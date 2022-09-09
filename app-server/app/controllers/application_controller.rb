f6e6e655f496df0ca4d03fc4a8946983b38e970f
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-09-09 17:09:29 -0400

    added the faker gem to gemfile

--- app-server/app/controllers/application_controller.rb
+++ app-server/app/controllers/application_controller.rb
@@ -3,8 +3,37 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/pets" do
-    pets = Pet.all
+    pets = Pet.all.order(:name)
     pets.to_json
   end
+
+  get "/appointments" do
+    appointments = Appointment.all.order(:on_date)
+    appointments.to_json(include: {pet: { only: [:name, :pet_type, :breed] }})
+  end
+
+
+  #need to add other data points
+  post "/appointments" do
+    #binding.pry
+    appointment = Appointment.create(
+      on_date: params[:on_date],
+      at_time: params[:at_time]
+    )
+    pet = Pet.create(name: params[:pet_name])
+    owner = Owner.create(first_name: params[:owner_name])
+    pet.appointments << appointment
+    owner.appointments << appointment
+    appointment.to_json
+  end
+
+  #working well
+  delete "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    appointment.to_json
+  end
+
+
 
 end
--- app-server/db/seeds.rb
+++ app-server/db/seeds.rb
@@ -1,5 +1,23 @@
+require 'faker'
+
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+10.times do 
+    Pet.create(
+        name: Faker::Creature::Cat.name,
+        pet_type: 'cat',
+        breed: Faker::Creature::Cat.breed,
+        dob: "#{rand(1..12)}/#{rand(1..28)}/#{rand(2009..2021)}/",
+        image_url: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg")
+    )
+end
+
+5.times do
+    Owner.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: "#{Faker::Camera.brand}@yahoo.com"
+    )
+end
 
 puts "✅ Done seeding!"
