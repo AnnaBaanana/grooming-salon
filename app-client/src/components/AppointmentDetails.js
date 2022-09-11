c6ed66e8359f20a6308e4991116f3ef4a2d1a73c
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-09-11 15:53:22 -0400

    added the state for edited appointment and the patch request to updated the edited appointment in the db

--- app-client/src/components/AppointmentDetails.js
+++ app-client/src/components/AppointmentDetails.js
@@ -4,11 +4,22 @@ function AppointmentDetails({editForm, formData, handleFormDataChange, handleFor
     function handleSubmit(e) {
         e.preventDefault()
         if (formData.on_date != null && formData.at_time != null ) {
-        fetch('http://localhost:9292/appointments', {
-        method: "POST",
-        headers: {"Content-type": "Application/json"},
-        body: JSON.stringify(formData)}).then(res => res.json()).then(data => 
-            handleFormSubmit(data))}
+            if (editForm>0) {
+                //if editForm is not NULL -- form is being edited -- do patch
+                fetch(`http://localhost:9292/appointments/${editForm}`, {
+                    method: "PATCH",
+                    headers: {"Content-type": "Application/json"},
+                    body: JSON.stringify(formData)}).then(res => res.json()).then(data => 
+                        handleFormSubmit(data))
+                    } else {
+                //else form is new -- do post
+                fetch('http://localhost:9292/appointments', {
+                    method: "POST",
+                    headers: {"Content-type": "Application/json"},
+                    body: JSON.stringify(formData)}).then(res => res.json()).then(data => 
+                        handleFormSubmit(data))
+            }
+            }
         else {
             alert("Appointment Date and Time cannot be blank")
         }
