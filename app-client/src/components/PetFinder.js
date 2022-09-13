e1772d220ea53b57c3ffefe4db433baba1ab94e2
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-09-13 16:08:31 -0400

    added the PetFinder component and put its state into the PetList to filter based on the search result

--- app-client/src/components/PetFinder.js
+++ app-client/src/components/PetFinder.js
@@ -1,9 +1,16 @@
-function PetFinder() {
+function PetFinder({findName, searchByName}) {
+    
+
     return(
         <div>
             <h4>Find Pet</h4>
             <form>
-                <input type='text' name='name_search' placeholder='search by name'/>
+                <input 
+                    type='text' 
+                    name='name_search' 
+                    placeholder='search by name' 
+                    value={findName}
+                    onChange={e => searchByName(e.target.value)}/>
                 <input type='text' name='phone_search'placeholder='search by phone'/>
             </form>
         </div>
--- app-client/src/components/PetList.js
+++ app-client/src/components/PetList.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Pet from './Pet';
 import PetFinder from './PetFinder';
 
 function PetList({pets, createAppointment}) {
+
+    const [findName, setFindName] = useState('ory');
+
+    function searchByName(name) {
+        console.log('searching for', name)
+        setFindName(name)
+    }
+
+    console.log('from PetList', findName)
+
+    const filteredPets = pets.filter(pet => pet.name!==null).filter(pet => pet.name.toLowerCase().includes(findName.toLowerCase()))
+    console.log(filteredPets)
+
+
     return (
         <div>
-            <PetFinder />
+            <PetFinder findName={findName} searchByName={searchByName}/>
             <div className='list'>
                 <ul>
-                    {pets.map((pet) => (
+                    {filteredPets.map((pet) => (
                         <Pet
                             key={pet.id}
                             pet={pet}
