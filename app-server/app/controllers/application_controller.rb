c63039297aec896d39bbaf05a884d0530e8eea11
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-09-19 11:23:07 -0400

    updated logic to create new appointment for existing pets

--- app-server/app/controllers/application_controller.rb
+++ app-server/app/controllers/application_controller.rb
@@ -14,16 +14,31 @@ class ApplicationController < Sinatra::Base
 
   #need to add other data points
   post "/appointments" do
-    #binding.pry
-    appointment = Appointment.create(
+    binding.pry
+    owner = Owner.find_or_create_by(phone: params[:owner_phone])
+    owner.update(first_name: params[:owner_first_name], last_name: params[:owner_last_name])
+    pet = owner.pets.find_or_create_by(name: params[:pet_name])
+    pet.update(breed: params[:breed], pet_type: params[:pet_type])
+    # if new appt is created that means owner--pet combo did not exist --> update appointment
+    #if old appt is found --> need to create new appt
+    if (owner.appointments.last==nil)
+      puts 'new appointment'
+      owner.appointments.last.update(
       on_date: params[:on_date],
       at_time: params[:at_time],
-      price: params[:price]
-    )
-    pet = Pet.create(name: params[:pet_name], breed: params[:breed], pet_type: params[:pet_type])
-    owner = Owner.create(first_name: params[:owner_first_name], last_name: params[:owner_last_name], phone: params[:owner_phone])
-    pet.appointments << appointment
-    owner.appointments << appointment
+      price: params[:price])
+    else
+      puts 'old appointment'
+      owner.appointments.create(
+        on_date: params[:on_date],
+        at_time: params[:at_time],
+        price: params[:price],
+        pet_id: pet.id
+      )
+    end
+    
+    appointment = owner.appointments.last
+  
     appointment.to_json(only: [:id, :on_date, :at_time, :price], include: {pet: {include: :owners}} )
   end
 
